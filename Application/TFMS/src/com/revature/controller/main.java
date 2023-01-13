7b9f687c347e4b94f9cde2e2785384575c27b2ed
ita_me_rush <rushilokhande78@gmail.com>
2023-01-13 19:14:19 +0530

    Application files added

---
+++ Application/TFMS/src/com/revature/controller/main.java
@@ -0,0 +1,13 @@
+package com.revature.controller;
+
+
+import com.revature.model.Associate;
+
+public class main {
+	public static void main(String [] args) {
+		Associate a=new Associate();
+	
+		a.setTrainee_name("Rushi");
+		System.out.println(a.getTrainee_name());
+	}
+}
